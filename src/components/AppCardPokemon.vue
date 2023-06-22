5e6cd303420e0d15f91ee2b827255b82bef1ced6
CraciDenisStefan <crdenis13@gmail.com>
2023-06-22 19:34:06 +0200

    [FEAT]Main completo

--- src/components/AppCardPokemon.vue
+++ src/components/AppCardPokemon.vue
@@ -6,19 +6,28 @@ export default {
 }
 </script>
 <template >
-  
-        <img :src="unPokemon.imageUrl" alt="">
-        <div>{{ unPokemon.number }}</div>
-        <div>{{ unPokemon.name }}</div>
-        <div>{{ unPokemon.type1 }}</div>
- 
-
-
+        <div class="rounded  p-3" :class="unPokemon.type1 === 'Fire' ?'bg_fire': unPokemon.type1 === 'Grass' ?'bg_grass': unPokemon.type1 === 'Water' ?'bg_water' : ''" >
+            <img :src="unPokemon.imageUrl" alt="">
+            <div>{{ unPokemon.number }}</div>
+            <div class="fw-bolder">{{ unPokemon.name }}</div>
+            <div>{{ unPokemon.type1 }}</div>
+        </div>
 </template>
 <style lang="scss" scoped>
 img{
-    width: 50%;
+    width: 60%;
     border-radius:50%;
+    height: 6rem;
+}
+
+.bg_fire{
+    background-color: rgb(250, 146, 146);
+}
+.bg_grass{
+    background: rgb(170, 245, 170);
+}
+.bg_water{
+    background-color: rgb(161, 235, 235);
 }
     
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,14 +15,17 @@ export default {
 }
 </script>
 <template >
-    <div class="container" >
-        <div class="row">
-            <div class="col-3"  v-for="(pokemon, index) in store.pokemons" :key="index" >
+    <div class="container rounded p-4 bg-secondary-subtle mt-5" >
+        <div class="row bg-secondary  py-4">
+            <div class=" col-6 col-md-4 col-lg-3 col-xxl-2 text-center mb-4 "  v-for="(pokemon, index) in store.pokemons" :key="index" >
             <AppCardPokemon :unPokemon="pokemon"/>
             </div>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    .row{
+        height: 30rem;
+        overflow: auto;
+    }
 </style>
