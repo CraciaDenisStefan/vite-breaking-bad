e2d62cc6e132ad2cccfd2bc5f7a6b3feab031a34
CraciDenisStefan <crdenis13@gmail.com>
2023-06-22 18:43:24 +0200

    Creata l'importazione corretta dei dati

--- src/components/AppCardPokemon.vue
+++ src/components/AppCardPokemon.vue
@@ -1,10 +1,24 @@
 <script>
 export default {
-    
+    props:{
+        unPokemon: Object
+    }
 }
 </script>
 <template >
+  
+        <img :src="unPokemon.imageUrl" alt="">
+        <div>{{ unPokemon.number }}</div>
+        <div>{{ unPokemon.name }}</div>
+        <div>{{ unPokemon.type1 }}</div>
+ 
+
+
 </template>
 <style lang="scss" scoped>
+img{
+    width: 50%;
+    border-radius:50%;
+}
     
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,27 @@
 <script>
+import {store} from '../store.js';
+
+import AppCardPokemon from './AppCardPokemon.vue'
+
 export default {
-    
+    components:{
+        AppCardPokemon,
+    },
+    data(){
+        return{
+            store,
+        }
+    },
 }
 </script>
 <template >
-    main
+    <div class="container" >
+        <div class="row">
+            <div class="col-3"  v-for="(pokemon, index) in store.pokemons" :key="index" >
+            <AppCardPokemon :unPokemon="pokemon"/>
+            </div>
+        </div>
+    </div>
 </template>
 <style lang="scss" scoped>
     
